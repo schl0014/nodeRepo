b21db6e9c7841e749b191d5c7c0ce70d6dd43265
niek726 <schl0014@hz.nl>
2023-01-19 11:24:12 +0100

    implemented image

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile('index.html', { root: "public" })
 })
 
 app.listen(port, () => {
